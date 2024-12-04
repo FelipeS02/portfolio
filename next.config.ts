1e5d45eb8efcf597a8d52c1f86ed63f5d812b72a
Franco <franco.matus@lilab.pe>
2024-12-04 12:51:38 -0300

    deshabilitado eslint no unused vars

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-  },
   images: {
     unoptimized: true,
   },
