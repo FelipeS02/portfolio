589fc05d34c05ed51de4a1ae62138bd6144a1d15
Felipe Saracho <franco.matus@lilab.pe>
2025-01-05 19:22:43 -0300

    fix import errors

--- app/api/theme/route.ts
+++ app/api/theme/route.ts
@@ -3,32 +3,21 @@ import { NextRequest } from 'next/server';
 import { getNewThemeByHex, getRandomHex } from '@/lib/theme';
 import { Theme } from '@/models/theme';
 
-let cachedTheme: Theme | null = null;
-
 export async function GET(req: NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
     const hexCode = searchParams.get('color');
-
-    let theme = cachedTheme;
 
     let newTheme: Theme | null = null;
 
     if (hexCode) {
       newTheme = await getNewThemeByHex(hexCode);
     } else {
-      if (!theme) {
-        newTheme = await getNewThemeByHex(getRandomHex());
-      }
-    }
-
-    if (newTheme) {
-      theme = newTheme;
-      cachedTheme = newTheme;
+      newTheme = await getNewThemeByHex(getRandomHex());
     }
 
     return Response.json({
-      data: { theme },
+      data: { theme: newTheme },
       status: 200,
       message: 'Image founded',
     });
