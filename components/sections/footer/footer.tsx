c7ab9d015b97e8b97b5408e9aebdc41fc0d42db2
Felipe Saracho <FelipeSaracho02@gmail.com>
2025-02-01 00:25:00 -0300

    cambios

--- components/sections/footer/footer.tsx
+++ components/sections/footer/footer.tsx
@@ -16,36 +16,33 @@ const Footer: FC<HTMLAttributes<HTMLDivElement>> = ({
   return (
     <footer
       className={cn(
-        'flex h-[60vh] flex-col items-center justify-between overflow-hidden bg-[#111111] text-palette-50',
+        'flex h-[60vh] flex-col items-end justify-between overflow-hidden bg-[#111111] text-palette-50',
         className,
       )}
       id={id}
       {...rest}
     >
-      <div className='flex w-full flex-wrap items-start justify-between px-4 py-3'>
-        <h6 className='text-xl'>
-          ©{currentYear} <span className='font-semibold'>FSARACHO.</span>
-        </h6>
-
-        <div className='inline-flex flex-col gap-4' id='socials'>
-          <ArrowButton
-            as='a'
-            href='https://www.linkedin.com/in/felipe-saracho/'
-            target='_blank'
-          >
-            Linkedin
-          </ArrowButton>
-          <ArrowButton
-            as='a'
-            href='https://www.linkedin.com/in/felipe-saracho/'
-            target='_blank'
-          >
-            Github
-          </ArrowButton>
-        </div>
+      <div className='inline-flex w-fit gap-4 px-4 py-3' id='socials'>
+        <ArrowButton
+          as='a'
+          href='https://www.linkedin.com/in/felipe-saracho/'
+          target='_blank'
+        >
+          Linkedin
+        </ArrowButton>
+        <ArrowButton
+          as='a'
+          href='https://www.linkedin.com/in/felipe-saracho/'
+          target='_blank'
+        >
+          Github
+        </ArrowButton>
       </div>
 
-      <FooterText />
+      <div className='flex flex-col'>
+        <span className='ml-3 text-xl font-medium'>©{currentYear}</span>
+        <FooterText />
+      </div>
     </footer>
   );
 };
