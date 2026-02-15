e7eb5f52c6d56f0d1be74148106abb5bdf5db43a
Felipe Saracho <FelipeSaracho02@gmail.com>
2026-02-14 23:25:12 -0300

    Delete commented code

--- components/common/full-width-text.tsx
+++ components/common/full-width-text.tsx
@@ -1,49 +1,5 @@
 'use client';
 
-import React, { useEffectEvent, useLayoutEffect, useRef } from 'react';
 import { ReactFitty } from 'react-fitty';
-
-import { Slot } from '@radix-ui/react-slot';
-
-// export default function FullWidthText({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   // const textRef = useRef<HTMLParagraphElement>(null);
-
-//   // const fitTextToContainer = useEffectEvent(() => {
-//   //   const element = textRef.current;
-//   //   const parent = element?.parentElement;
-
-//   //   if (!element || !parent) return;
-
-//   //   const parentWidth = parent?.clientWidth;
-//   //   // restablece transform para medición limpia
-//   //   element.style.transform = 'scaleX(1)';
-//   //   const textWidth = element.scrollWidth;
-
-//   //   const scale = parentWidth / textWidth;
-//   //   console.log(scale);
-
-//   //   if (scale > 1) {
-//   //     element.style.transform = `scale(${scale})`;
-//   //   } else {
-//   //     element.style.transform = 'scale(1)';
-//   //   }
-//   // });
-
-//   // useLayoutEffect(() => {
-//   //   window.addEventListener('resize', fitTextToContainer);
-
-//   //   fitTextToContainer();
-
-//   //   return () => {
-//   //     window.removeEventListener('resize', fitTextToContainer);
-//   //   };
-//   // }, [children]);
-
-//   return <ReactFitty>{children}</ReactFitty>;
-// }
 
 export default ReactFitty;
